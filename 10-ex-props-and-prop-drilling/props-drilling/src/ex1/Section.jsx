f678771a22cf7b593fbdbc5f8487e7190675c48a
DayVee <dreyimbaev02@gmail.com>
2024-03-05 20:23:01 +0500

    10

---
+++ 10-ex-props-and-prop-drilling/props-drilling/src/ex1/Section.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header.jsx";
+import Paragraph from "./Paragraph.jsx";
+
+function Section() {
+  return (
+    <div>
+      <Header text='Exercise 1!' />
+      <Paragraph text='I love monkeys!' />
+    </div>
+  )
+}
+
+export default Section;
---
+++ 10-ex-props-and-prop-drilling/props-drilling/src/warmup/Section.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header.jsx";
+import Paragraph from "./Paragraph.jsx";
+
+function Section() {
+  return (
+    <div>
+      <Header />
+      <Paragraph />
+    </div>
+  )
+}
+
+export default Section;
