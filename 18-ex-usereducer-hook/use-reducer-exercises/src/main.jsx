c4cdaf52d383a7577326d125a28a814cae2bd972
DayVee <dreyimbaev02@gmail.com>
2024-03-17 11:27:06 +0500

    18

---
+++ 18-ex-usereducer-hook/use-reducer-exercises/src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import Counter from './ex2/Counter.jsx'
+import ControlledForm from './ex3/ControlledForm.jsx'
+// import Counter from './ex1/Counter.jsx'
+// import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ControlledForm />
+  </React.StrictMode>,
+)
