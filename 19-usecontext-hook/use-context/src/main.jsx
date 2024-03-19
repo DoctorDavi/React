b67c01d241478b25e4d5c2d0dbd93e02767883f5
DayVee <dreyimbaev02@gmail.com>
2024-03-19 13:57:31 +0500

    19

---
+++ 19-usecontext-hook/use-context/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Level3 from './Level3.jsx'
+// import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Level3 />
+  </React.StrictMode>,
+)
